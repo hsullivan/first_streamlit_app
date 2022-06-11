abffb4688cefea4d75e521ddb112df0a66f8c641
hsullivan <hsullivan6@gmail.com>
2022-06-11 14:19:09 -0400

    Update streamlit_app.py
    snowflake connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,3 +35,10 @@ streamlit.dataframe(fruityvice_normalized)
 
 
 import snowflake.connector
+
+my_cnx = snowflake.connector.conect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("HEllow from Snowflake:")
+streamlit.rext(my_data_row)
