df5283ccd292df9f0b477dfb021063150215eef3
hsullivan <hsullivan6@gmail.com>
2022-06-11 14:30:27 -0400

    Update streamlit_app.py
    added new text box

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ streamlit.dataframe(fruits_to_show)
 #New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('what fruit would you like information about?','Kiwi')
-streamlit.write('The user endteered', fruit_choice)
+streamlit.write('The user entered', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
@@ -42,3 +42,6 @@ my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
+
+add_my_fruit = streamlit.text_input('what fruit would you like to add?','jackfruit')
+streamlit.write('The user entered', add_my_fruit)
